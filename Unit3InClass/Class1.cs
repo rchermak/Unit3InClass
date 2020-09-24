5377b1527eaeb978d18f6a900de67e1076ad73d0
rchermak <rchermak@LABCIS01-2S2P>
2020-09-24 10:33:23 -0500

    new change

---
+++ Unit3InClass/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit3InClass
+{
+    class Class1
+    {
+        DateTime saleTime;
+        float amount;
+
+        public Class1(float amount)
+        {
+            this.amount = amount;
+        }
+
+        public DateTime SaleTime { get => saleTime; set => saleTime = value; }
+    }
+}
--- Unit3InClass/Program.cs
+++ Unit3InClass/Program.cs
@@ -7,6 +7,8 @@ namespace Unit3InClass
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Justice for Breonna Taylor");
         }
     }
 }
